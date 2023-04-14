e0a888544d2ab692880a01401e4ffe9f116791aa
James Mitsumasu <jrmitsumasu@gmail.com>
2023-04-13 20:32:44 -0400

    Complete
    Added dependecy via maven and completed end-points along with logic

---
+++ src/main/java/com/coderscampus/assignment9/FileReaderService.java
@@ -0,0 +1,46 @@
+package com.coderscampus.assignment9;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FileReaderService {
+
+    private List<Recipe> recipes = new ArrayList<>();
+
+
+    Recipe recipe;
+
+    public FileReaderService() throws IOException {
+        Reader in = new FileReader("recipes.txt");
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().withIgnoreSurroundingSpaces().withEscape('\\').parse(in);
+        for (CSVRecord record : records) {
+            recipe = new Recipe();
+            recipe.setCookingMinutes(Integer.parseInt(record.get(0)));
+            recipe.setDairyFree(Boolean.parseBoolean(record.get(1)));
+            recipe.setGlutenFree(Boolean.parseBoolean(record.get(2)));
+            recipe.setInstructions(record.get(3));
+            recipe.setPreparationMinutes(Double.parseDouble(record.get(4)));
+            recipe.setPricePerServing(Double.parseDouble(record.get(5)));
+            recipe.setReadyInMinutes(Integer.parseInt(record.get(6)));
+            recipe.setServings(Integer.parseInt(record.get(7)));
+            recipe.setSpoonacularScore(Double.parseDouble(record.get(8)));
+            recipe.setTitle(record.get(9));
+            recipe.setVegan(Boolean.parseBoolean(record.get(10)));
+            recipe.setVegetarian(Boolean.parseBoolean(record.get(11)));
+            recipes.add(recipe);
+        }
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+}
---
+++ src/main/java/com/coderscampus/assignment9/RecipeController.java
@@ -0,0 +1,41 @@
+package com.coderscampus.assignment9;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class RecipeController {
+
+    @Autowired
+    FileReaderService fileReaderService;
+
+
+    @GetMapping("/gluten-free")
+    public List<Recipe> glutenFreeRecipes() {
+        return fileReaderService.getRecipes().stream().filter(Recipe::getGlutenFree).collect(Collectors.toList());
+    }
+
+    @GetMapping("/vegan")
+    public List<Recipe> veganRecipes() {
+        return fileReaderService.getRecipes().stream().filter(Recipe::getVegan).collect(Collectors.toList());
+    }
+
+    @GetMapping("/vegan-and-gluten-free")
+    public List<Recipe> veganAndGlutenFreeRecipes() {
+        return fileReaderService.getRecipes().stream().filter(Recipe::getGlutenFree).filter(Recipe::getVegan).collect(Collectors.toList());
+    }
+
+    @GetMapping("/vegetarian")
+    public List<Recipe> vegetarianRecipes() {
+        return fileReaderService.getRecipes().stream().filter(Recipe::getVegetarian).collect(Collectors.toList());w
+    }
+
+    @GetMapping("/all-recipes")
+    public List<Recipe> allRecipes() {
+        return fileReaderService.getRecipes();
+    }
+}
